c5f03476cb7bfd7dd03847d23e3c9b8652b9e9b6
Tue Hoang Trong <tuehoang07@gmail.com>
2017-07-19 14:44:40 +0800

    fix stock symbol regex

--- src/matcher/StockSymbol.js
+++ src/matcher/StockSymbol.js
@@ -24,7 +24,7 @@ Autolinker.matcher.StockSymbol = Autolinker.Util.extend( Autolinker.matcher.Matc
 	 * @property {RegExp} matcherRegex
 	 */
 	// matcherRegex : new RegExp( '[\$]{1}[.' + Autolinker.RegexLib.alphaNumericCharsStr + ']{1,12}', 'g' ),
-	matcherRegex : new RegExp( '[\$][' + Autolinker.RegexLib.alphaNumericCharsStr + ']{1,7}(.[a-zA-Z]{1,3})?', 'g' ),
+	matcherRegex : new RegExp( '[\$][' + Autolinker.RegexLib.alphaNumericCharsStr + ']{1,7}(\.[a-zA-Z]{1,3})?', 'g' ),
 
 	/**
 	 * The regular expression to use to check the character before a username match to
