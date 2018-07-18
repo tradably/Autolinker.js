b660936c8cc9af488d1976d739f6f6e84db19a5f
Xiaoguo <xiaoguo.pok@goldenequator.com>
2018-07-18 15:00:36 +0800

    changes

--- dist/Autolinker.js
+++ dist/Autolinker.js
@@ -2608,7 +2608,8 @@ Autolinker.match.Hashtag = Autolinker.Util.extend(Autolinker.match.Match, {
 				return 'https://www.facebook.com/hashtag/' + hashtag;
 			case 'instagram':
 				return 'https://instagram.com/explore/tags/' + hashtag;
-
+			case 'tradably':
+				return 'https://app.tradably.com/explore/tags/'+hashtag;
 			default:
 				// Shouldn't happen because Autolinker's constructor should block any invalid values, but just in case.
 				throw new Error('Unknown service name to point hashtag to: ', serviceName);
